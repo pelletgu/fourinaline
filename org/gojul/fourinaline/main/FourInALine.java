546770480c8bd63282fe08a0bf98097a27b06c94
julien_aubin_lm <julien_aubin_lm@yahoo.fr>
2008-03-28 23:51:14 +0000

    Fixed a warning in the source code.

--- org/gojul/fourinaline/main/FourInALine.java
+++ org/gojul/fourinaline/main/FourInALine.java
@@ -27,8 +27,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.gojul.fourinaline.model.GameServerImpl;
-
 /**
  * The program launcher, from the user point of view.
  *
